880052e8dc5f9100a92293c43fa8ea0f5cafa690
sythenA <sythena@gmail.com>
2017-09-29 15:37:36 +0800

    2017/9/29 15:35 Build
    # Add selector dialog to vncviewer.exe and NTOUHydro execute .bat
    # Add settings file, load settings before calling models.

--- startModels.py
+++ startModels.py
@@ -2,12 +2,13 @@
 
 import os
 import _winreg
+import re
 from PyQt4 import QtGui, uic
 import qgis.utils
 import subprocess
 import MBFace
 from selectorDiag import netSelector
-from PyQt4.QtGui import QMessageBox
+from PyQt4.QtGui import QMessageBox, QFileDialog
 from infoDiag import infoView
 
 
@@ -26,8 +27,28 @@ class callModels:
     def __init__(self, iface, model='SRH'):
         self.iface = iface
         self.plugin_dir = os.path.dirname(__file__)
-
+        self.readSetting()
         self.dlg = callModelsPopUp()
+
+        try:
+            self.vncPath = self.settings['VncViewerPath']
+        except:
+            try:
+                reg = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
+                k = _winreg.OpenKey(
+                    reg, r'SOFTWARE\Classes\VncViewer.Config')
+                pathKey = _winreg.EnumKey(k, 0)
+                pathName = _winreg.QueryValue(k, pathKey)
+                pathName = pathName.split(',')[0]
+                self.vncPath = pathName
+                self.settings.update({'VncViewerPath': self.vncPath})
+
+                f = open(os.path.join(self.plugin_dir, 'settings'), 'w')
+                for key in self.settings.keys():
+                    f.write(key + ' = ' + self.settings[key] + '\n')
+                f.close()
+            except:
+                self.noVnc()
 
         if model == 'CCHE':
             self.callCCHE()
@@ -52,54 +73,72 @@ class callModels:
             self.dlg.init2D.clicked.connect(self.callNTOUHydro_WRA)
             self.dlg.init3D.clicked.connect(self.callNTOUHydro_author)
 
-        self.vncPath = ''
-        try:
-            reg = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
-            k = _winreg.OpenKey(
-                reg, r'SOFTWARE\Classes\VncViewer.Config')
-            pathKey = _winreg.EnumKey(k, 0)
-            pathName = _winreg.QueryValue(k, pathKey)
-            pathName = pathName.split(',')[0]
-            self.vncPath = pathName
-        except:
-            title = u'找不到VncViewer'
-            message = u'在系統中找不到已安裝的VncViewer\n將無法啟用需要遠端連線\
-的功能'
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Warning)
-            msg.setText(message)
-            msg.setWindowTitle(title)
-            msg.setStandardButtons(QMessageBox.Ok)
-            msg.exec_()
-
     def callNTOUHydro(self):
         self.dlg.init3D.setVisible(True)
         self.dlg.init1D.setText(u'本地端使用\n(限水規所內網域)')
         self.dlg.init2D.setText(u'遠端連線使用\n(限水利署內網域)')
         self.dlg.init3D.setText(u'申請使用權限')
 
+    def readSetting(self):
+        try:
+            f = open(os.path.join(self.plugin_dir, 'settings'), 'r')
+            lines = f.readlines()
+            setting = dict()
+            for line in lines:
+                key = re.split('=', line)[0]
+                content = re.split('=', line)[1]
+                setting.update({key: content})
+            f.close()
+            self.settings = setting
+        except:
+            setting = dict()
+            self.settings = setting
+
     def callNTOUHydro_WRAP(self):
         try:
-            reg = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
-            key = _winreg.OpenKey(
-                reg, r'SOFTWARE\Wow6432Node\MicrosoftWindows\CurrentVersion\Uni\
-nstall\QGIS Wien\DisplayIcon')
-            pathKey = _winreg.EnumKey(key, 0)
-            pathName = _winreg.QueryValue(key, pathKey)
-
-            basePath = os.path.dirname(os.path.dirname(pathName))
-            batPath = os.path.join(basePath, 'bin', 'qgis-ltr.bat')
-
+            batPath = self.settings['NTOUHydro']
             subprocess.Popen([batPath])
         except:
-            title = u'找不到水文水理分析系統'
-            message = u'找不到水文水理分析系統\n請先至「申請權限」網頁下載軟體'
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setText(message)
-            msg.setWindowTitle(title)
-            msg.setStandardButtons(QMessageBox.Ok)
-            msg.exec_()
+            try:
+                reg = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
+                key = _winreg.OpenKey(
+                    reg, r'SOFTWARE\Wow6432Node\MicrosoftWindows\CurrentVersion\
+Uninstall\QGIS Wien\DisplayIcon')
+                pathKey = _winreg.EnumKey(key, 0)
+                pathName = _winreg.QueryValue(key, pathKey)
+
+                basePath = os.path.dirname(os.path.dirname(pathName))
+                batPath = os.path.join(basePath, 'bin', 'qgis-ltr.bat')
+
+                self.settings.update({'NTOUHydro': batPath})
+                f = open(os.path.join(self.plugin_dir, 'settings'), 'w')
+                for key in self.settings.keys():
+                    f.write(key + ' = ' + self.settings[key] + '\n')
+                f.close()
+
+                subprocess.Popen([batPath])
+            except:
+                title = u'找不到水文水理分析系統'
+                message = u'找不到水文水理分析系統\n請先至「申請權限」網頁下載\
+軟體'
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText(message)
+                msg.setWindowTitle(title)
+                msg.setStandardButtons(QMessageBox.Ok)
+                msg.exec_()
+
+                Caption = u'請指定水文水理分析系統的執行路徑(qgis-ltr.bat)'
+                fileName = QFileDialog.getOpenFileName(self.dlg, Caption, '',
+                                                       "*.bat")
+                if fileName:
+                    self.ntouHydroPath = fileName.encode('big5')
+                    self.settings.update({'NTOUHydro': self.ntouHydroPath})
+
+                    f = open(os.path.join(self.plugin_dir, 'settings'), 'w')
+                    for key in self.settings.keys():
+                        f.write(key + ' = ' + self.settings[key] + '\n')
+                    f.close()
 
     def noVnc(self):
         title = u'找不到VncViewer'
@@ -111,14 +150,25 @@ nstall\QGIS Wien\DisplayIcon')
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec_()
 
+        Caption = u'請指定UltraVNC Viewer執行檔(vncviewer.exe)的路徑'
+        fileName = QFileDialog.getOpenFileName(self.dlg, Caption, '', "*.exe")
+        if fileName:
+            self.vncPath = fileName.encode('big5')
+            self.settings.update({'VncViewerPath': self.vncPath})
+
+            f = open(os.path.join(self.plugin_dir, 'settings'), 'w')
+            for key in self.settings.keys():
+                f.write(key + ' = ' + self.settings[key] + '\n')
+            f.close()
+
     def callNTOUHydro_WRA(self):
         if self.vncPath:
             self.dlg.done(0)
             subprocess.Popen([self.vncPath, '210.69.15.31::5999',
                              '-user', '07-gpu', '-password', '1!qaz2@wsx'])
+            self.dlg.done(0)
         else:
             self.noVnc()
-        self.dlg.done(0)
 
     def callNTOUHydro_author(self):
         os.system('start http://emh-123.wrap.gov.tw/wagis/river/index.html')
@@ -203,6 +253,18 @@ ew?usp=sharing'
             toolBar = MBFace.callMB(self.iface, self.MBModel)
             self.dlg.done(0)
             toolBar.run()
+        else:
+            title = u'請先安裝SRH2D Mesh Builder QGIS插件'
+            message = u'SRH2D Mesh Builder插件尚未安裝\n請從關閉視窗後開啟的連\
+結下載SRH2D Mesh Builder安裝包'
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText(message)
+            msg.setWindowTitle(title)
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+            os.system('start https://drive.google.com/a/manysplendid.com/file/d\
+/0BwtvbTG03hXKR3NHeGEyYUl0T0U/view?usp=sharing')
 
     def callSRH3D(self):
         infoViewer = infoView(self.iface, 104)
@@ -211,9 +273,9 @@ ew?usp=sharing'
             self.dlg.done(0)
             subprocess.Popen([self.vncPath, '210.69.15.31::5999',
                              '-user', '07-gpu', '-password', '1!qaz2@wsx'])
+            self.dlg.done(0)
         else:
             self.noVnc()
-        self.dlg.done(0)
 
     def run(self):
         result = self.dlg.exec_()
